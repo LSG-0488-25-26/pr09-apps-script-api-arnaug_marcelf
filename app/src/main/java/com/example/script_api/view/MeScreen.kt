c9649733c3c9453a8f973bc16754c6a0e003d70d
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-03-18 19:59:48 +0100

    feat: meScreen updated

--- app/src/main/java/com/example/script_api/view/MeScreen.kt
+++ app/src/main/java/com/example/script_api/view/MeScreen.kt
@@ -1,2 +1,56 @@
 package com.example.script_api.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.script_api.viewModel.PandemicsViewModel
+import com.example.script_api.nav.Routes
+import com.example.script_api.viewModel.ScaffoldViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MeScreen(
+    viewModel: PandemicsViewModel,
+    navController: NavHostController,
+    scaffoldViewModel: ScaffoldViewModel
+) {
+    Scaffold(
+        topBar = {},
+        bottomBar = { MyBottomBar(scaffoldViewModel, navController) },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .padding(paddingValues), // respecta padding del scaffold
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Perfil d'Usuari", style = MaterialTheme.typography.headlineMedium)
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text("Usuari: ${viewModel.username}")
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Correu: ${viewModel.correu}")
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Button(
+                    onClick = {
+                        viewModel.logout()
+                        navController.navigate(Routes.Login.route)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Logout")
+                }
+            }
+        }
+    )
+}
