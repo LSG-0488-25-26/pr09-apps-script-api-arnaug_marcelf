1e16b1c5a8d292af988d231ac8afc28520589cc9
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-03-18 19:53:07 +0100

    feat: getters i setters

--- app/src/main/java/com/example/script_api/sharedPrefences/SettingsRepository.kt
+++ app/src/main/java/com/example/script_api/sharedPrefences/SettingsRepository.kt
@@ -12,4 +12,29 @@ class SettingsRepository {
         this.context = context
         this.sharedPreferences = context.getSharedPreferences(nomFitxer, Context.MODE_PRIVATE)
     }
+
+    fun <T> saveSettingValue(key: String, value: T) {
+        with(sharedPreferences.edit()) {
+            when (value) {
+                is String -> putString(key, value)
+                is Int -> putInt(key, value)
+                is Boolean -> putBoolean(key, value)
+                is Float -> putFloat(key, value)
+                is Long -> putLong(key, value)
+                else -> throw IllegalArgumentException("Unsupported type")
+            }
+            apply()
+        }
+    }
+
+    fun <T> getSettingValue(key: String, defaultValue: T): T {
+        return when (defaultValue) {
+            is String -> sharedPreferences.getString(key, defaultValue) as T
+            is Int -> sharedPreferences.getInt(key, defaultValue) as T
+            is Boolean -> sharedPreferences.getBoolean(key, defaultValue) as T
+            is Float -> sharedPreferences.getFloat(key, defaultValue) as T
+            is Long -> sharedPreferences.getLong(key, defaultValue) as T
+            else -> throw IllegalArgumentException("Unsupported type")
+        }
+    }
 }
